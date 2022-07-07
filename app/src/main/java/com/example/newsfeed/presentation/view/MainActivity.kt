5842867d9753136cd5dbb4a2671f5aed698d45f4
GetRighhttt <stefanbayne@gmail.com>
2022-07-07 19:21:19 -0400

    New commit.

--- app/src/main/java/com/example/newsfeed/presentation/view/MainActivity.kt
+++ app/src/main/java/com/example/newsfeed/presentation/view/MainActivity.kt
@@ -2,11 +2,30 @@ package com.example.newsfeed.presentation.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.newsfeed.R
+import com.example.newsfeed.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    /*
+    Construct view binding instance.
+     */
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        /*
+        Set up nav controller and nav host fragment with bottom navigation bar.
+         */
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNavView.setupWithNavController(navController)
     }
 }
